7842802e6778c0b58436f58ecec1da160b4c11c0
Anton Skshidlevsky <meefik@gmail.com>
2015-02-21 23:47:12 +0300

    Bugfix "Couldn't find these debs: 0"

--- jni/Android.mk
+++ jni/Android.mk
@@ -2,7 +2,8 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_CFLAGS   = -Wall -g -fPIE
+LOCAL_CFLAGS   = -static
+LOCAL_LDFLAGS  = -static
 
 LOCAL_MODULE    := pkgdetails
 LOCAL_SRC_FILES := pkgdetails.c
