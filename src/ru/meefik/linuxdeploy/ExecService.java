2b81f834708640b6a420a4a145bc1d134a74ff74
Anton Skshidlevsky <meefik@gmail.com>
2015-06-09 23:54:34 +0300

    Update bootup/shutdown
    Former-commit-id: 09d6233a520d53cf1166f438b00a6141af43467b

--- src/ru/meefik/linuxdeploy/ExecService.java
+++ src/ru/meefik/linuxdeploy/ExecService.java
@@ -11,15 +11,15 @@ public class ExecService extends Service {
 	public IBinder onBind(Intent arg0) {
 		return null;
 	}
-	
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-    	Context mContext = this;
-    	String command = intent.getStringExtra("command");
-    	ShellEnv env = new ShellEnv(mContext);
-    	env.execScript(command);
-    	MainActivity.notification(mContext, null);
-    	return super.onStartCommand(intent, flags, startId);
-    }
+
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		Context mContext = this;
+		MainActivity.notification(mContext, null);
+		String command = intent.getStringExtra("command");
+		ShellEnv env = new ShellEnv(mContext);
+		env.execScript(command);
+		return super.onStartCommand(intent, flags, startId);
+	}
 
 }
