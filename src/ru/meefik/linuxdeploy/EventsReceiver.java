10b977dbacbea00b6f65e0e7ef2b124bcd9184ee
Anton Skshidlevsky <meefik@gmail.com>
2015-06-09 23:33:19 +0300

    Update bootup/shutdown actions
    Former-commit-id: 1a129c2687acdbe13de88847d291d1db79e100a5

---
+++ src/ru/meefik/linuxdeploy/EventsReceiver.java
@@ -0,0 +1,24 @@
+package ru.meefik.linuxdeploy;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class EventsReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(final Context context, Intent intent) {
+		Intent service = new Intent(context, ExecService.class);
+        if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
+    		service.putExtra("command", "start");
+    		context.startService(service);
+    		return;
+        }
+        if (intent.getAction().equals(Intent.ACTION_SHUTDOWN)) {
+    		service.putExtra("command", "stop");
+    		context.startService(service);
+    		return;
+        }  
+	}
+
+}
