97ac141a992f5e9b2a15f03dc7f57d65f9465be3
Anton Skshidlevsky <meefik@gmail.com>
2015-10-02 23:36:29 +0300

    Improved compatibility

--- contrib/tools/root-avd.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-[ -z "$1" ] && { echo "Usage: $0 <path/to/su>"; exit 1; }
-alias adb=$HOME/android-sdk-linux/platform-tools/adb
-adb shell "mount -o rw,remount /system"
-adb shell "rm -rf /system/media/audio"
-adb push "$1" /system/bin/su
-adb shell "chown 0:0 /system/bin/su"
-adb shell "chmod 6755 /system/bin/su"
