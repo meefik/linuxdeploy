4c0a59b204f734e742afbf7824a1ef8ce5e40abd
rkkoszewski <rkkoszewski@gmail.com>
2016-10-27 10:56:39 +0200

    Autostart Delay Implementation

--- app/src/main/java/ru/meefik/linuxdeploy/BootReceiver.java
+++ app/src/main/java/ru/meefik/linuxdeploy/BootReceiver.java
@@ -10,6 +10,13 @@ public class BootReceiver extends BroadcastReceiver {
     public void onReceive(final Context context, Intent intent) {
         switch (intent.getAction()) {
             case Intent.ACTION_BOOT_COMPLETED:
+                try { // Auto start Delay
+                    Integer delay_s = PrefStore.getAutostartDelay(context);
+                    // Logger.log(context, "AUTO START DELAY: Waiting for "+delay_s+"s");
+                    Thread.sleep(delay_s * 1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
                 EnvUtils.execService(context, "start", "-m");
                 break;
             case Intent.ACTION_SHUTDOWN:
