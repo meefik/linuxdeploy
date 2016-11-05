b10b508fcc231143492075a1c70af38c57a543c0
Anton Skshidlevsky <meefik@gmail.com>
2016-11-05 16:09:35 +0300

    Updated unchroot

--- include/core/unchroot/deploy.sh
+++ include/core/unchroot/deploy.sh
@@ -5,16 +5,17 @@
 do_configure()
 {
     msg ":: Configuring ${COMPONENT} ... "
-    local unchroot="${CHROOT_DIR}/bin/unchroot"
+    local unchroot="${CHROOT_DIR}/sbin/unchroot"
     echo '#!/bin/sh' > "${unchroot}"
     echo 'if [ "$(whoami)" != "root" ]; then' >> "${unchroot}"
     echo 'sudo $0 "$@"; exit $?' >> "${unchroot}"
     echo 'fi' >> "${unchroot}"
-    echo "PATH=$PATH:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin" >> "${unchroot}"
+    echo 'chroot_exec=$(which chroot)' >> "${unchroot}"
+    echo "PATH=$PATH" >> "${unchroot}"
     echo 'if [ $# -eq 0 ]; then' >> "${unchroot}"
-    echo 'chroot /proc/1/cwd su -' >> "${unchroot}"
+    echo '$chroot_exec /proc/1/cwd su -' >> "${unchroot}"
     echo 'else' >> "${unchroot}"
-    echo 'chroot /proc/1/cwd "$@"' >> "${unchroot}"
+    echo '$chroot_exec /proc/1/cwd "$@"' >> "${unchroot}"
     echo 'fi' >> "${unchroot}"
     chmod 755 "${unchroot}"
     return 0
