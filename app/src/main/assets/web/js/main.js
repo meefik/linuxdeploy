9e5fac1faf62791a972a8718212416a89ef52105
meefik <meefik@gmail.com>
2020-01-02 04:16:51 +0300

    Updated web terminal

--- app/src/main/assets/web/js/main.js
+++ app/src/main/assets/web/js/main.js
@@ -46,11 +46,14 @@ window.onload = function () {
   var socketURL = protocol + location.hostname + ((port) ? (':' + port) : '');
   var socket = new WebSocket(socketURL);
 
-  Terminal.applyAddon(fit);
-
-  var xterm = new Terminal({ cursorBlink: true, fontSize: getQueryParams('size') });
-  xterm.open(document.getElementById('terminal'));
-  xterm.fit();
+  var terminal = new Terminal({
+    cursorBlink: true,
+    fontSize: getQueryParams('size') || 16
+  });
+  var fitAddon = new FitAddon.FitAddon();
+  terminal.loadAddon(fitAddon);
+  terminal.open(document.getElementById('terminal'));
+  fitAddon.fit();
 
   socket.addEventListener('message', function (ev) {
     blobToText(ev.data, function (str) {
@@ -58,31 +61,30 @@ window.onload = function () {
         var match = str.match(/\/dev\/pts\/\d+/);
         if (match) {
           pty = match[0];
-          resizePty(pty, xterm.rows, xterm.cols);
+          resizePty(pty, terminal.rows, terminal.cols);
         }
       }
       str = str.replace(/([^\r])\n|\r$/g, '\r\n');
-      xterm.write(str);
+      terminal.write(str);
     });
   });
 
-  xterm.on('data', function (data) {
+  terminal.onData(function (data) {
     socket.send(textToBlob(data));
   });
 
-  xterm.on('resize', function (e) {
+  terminal.onResize(function (e) {
     resizePty(pty, e.rows, e.cols);
   });
 
   window.addEventListener('resize', function () {
-    xterm.fit();
+    fitAddon.fit();
   });
 
   // Hot key for resize: Ctrl + Alt + r
   window.addEventListener('keydown', function (e) {
     if (e.ctrlKey && e.altKey && e.keyCode == 82) {
-      resizePty(pty, xterm.rows, xterm.cols, true);
+      resizePty(pty, terminal.rows, terminal.cols, true);
     }
   });
 };
-
